4eee8fc08d132d35b604d3dd73cf90dec5de9673
Noa Kim <noanonoa@gmail.com>
2021-05-11 15:40:25 -0700

    App successfully calls on pokeAPI for list of original 150 pokemons and drills it to Pokedex

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Pokedex from './components/Pokedex/Pokedex'
+import { getOriginalPokemonList } from './api/utils'
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([])
+
+  useEffect(() => {
+    async function loadPokemonList(){
+      const results = await getOriginalPokemonList()
+
+      setPokemonList(results)
+    }
+
+    loadPokemonList()
+  }, [])
+
   return (
     <div className="App">
-      <Pokedex />
+      <Pokedex
+        pokemonList={pokemonList}
+      />
     </div>
   );
 }
