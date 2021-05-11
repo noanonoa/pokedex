e103d3600bfe4fce0977d91788c432d71f0c2047
Noa Kim <noanonoa@gmail.com>
2021-05-11 15:44:42 -0700

    PokeSelect renders all pokemon names successfully

--- src/components/PokeSelect/PokeSelect.jsx
+++ src/components/PokeSelect/PokeSelect.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import styles from './PokeSelect.module.css'
 
-function PokeSelect(){
+function PokeSelect({ pokemonList }){
+  const pokemons = pokemonList.map((pokemon, index) => {
+    return (
+      <option
+        key={index}
+        value={index + 1}
+      >
+        {pokemon.name}
+      </option>
+    )
+  })
+
   return (
     <div className={styles.PokeSelect}>
       <label htmlFor="pokemon">Choose a Pokemon:</label>
-      <select name="pokemon" id="pokemon">
+      <select>
+        {pokemons}
       </select>
     </div>
   )
