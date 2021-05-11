0f35bf4f0b8bfb9e74386840706d3ec44d5cda94
Noa Kim <noanonoa@gmail.com>
2021-05-11 14:20:45 -0700

    added element for display Pokemon name

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -7,6 +7,7 @@ function Pokemon(){
       <div className={styles['pokemon-image-container']}>
         <img src="https://www.fillmurray.com/200/200" alt="placeholder" />
       </div>
+      <h2 className={styles['pokemon-name']}>Pokemon Name</h2>
       <div className={styles['pokemon-description-container']}>
         Pokemon Description Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti similique sint doloremque nulla dicta soluta tempore eaque reiciendis provident maiores?
       </div>
