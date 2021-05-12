873eadc2b5deeee7abde30d1b90a54896a358d13
Noa Kim <noanonoa@gmail.com>
2021-05-11 17:13:48 -0700

    pokemon name formatting successfully

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import styles from './Pokemon.module.css'
+import { getPokemonImage } from '../../api/utils'
 
-function Pokemon(){
-  return (
-    <div className={styles.Pokemon}>
-      <div className={styles['pokemon-image-container']}>
-        <img src="https://www.fillmurray.com/200/200" alt="placeholder" />
+function Pokemon({ pokemonList, currentPokemonId }){
+  if (pokemonList.length > 0) {
+    const pokemonName = formatName(pokemonList[currentPokemonId - 1].name)
+
+    function formatName(name){
+      return name.slice(0,1).toUpperCase() + name.slice(1)
+    }
+
+    return (
+      <div className={styles.Pokemon}>
+        <div className={styles['pokemon-image-container']}>
+          <img src={getPokemonImage(currentPokemonId)} alt="placeholder" />
+        </div>
+        <h2 className={styles['pokemon-name']}>
+          {pokemonName}
+        </h2>
+        <div className={styles['pokemon-description-container']}>
+          Pokemon Description Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti similique sint doloremque nulla dicta soluta tempore eaque reiciendis provident maiores?
+        </div>
       </div>
-      <h2 className={styles['pokemon-name']}>Pokemon Name</h2>
-      <div className={styles['pokemon-description-container']}>
-        Pokemon Description Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti similique sint doloremque nulla dicta soluta tempore eaque reiciendis provident maiores?
-      </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className={styles.Pokemon}/>
+    )
+  }
 }
 
 export default Pokemon
