e38db380b721a86dfd1f3169e5ba6b681886463f
Noa Kim <noanonoa@gmail.com>
2021-05-12 18:46:00 -0700

    refactor code on App level

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Pokedex from './components/Pokedex/Pokedex'
-import { getOriginalPokemonList } from './api/utils'
 
 function App() {
-  const [pokemonList, setPokemonList] = useState([])
-  const [currentPokemonId, setCurrentPokemonId] = useState(1)
-
-  useEffect(() => {
-    async function loadPokemonList(){
-      const results = await getOriginalPokemonList()
-
-      setPokemonList(results)
-    }
-
-    loadPokemonList()
-  }, [])
-  
   return (
     <div className="App">
-      <Pokedex
-        currentPokemonId={currentPokemonId}
-        setCurrentPokemonId={setCurrentPokemonId}
-        pokemonList={pokemonList}
-      />
+      <Pokedex/>
     </div>
   );
 }
